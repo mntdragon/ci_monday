9ce824d49c5c1e313bd5d5599edf0b1c95ce5f14
Unknown <mntdragon96@gmail.com>
2017-01-30 15:22:25 +0200

    them hinh huong dan

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -8,7 +8,7 @@
 
 <h1>This is the CodeIgniter Example</h1>
 <ul>
-<li><a href="<?php echo site_url('customer/main');?>">Main Page</a></li> <!--main hoac main/index vi index se dong automaticlly-->
+<li><a href="<?php echo site_url('main');?>">Main Page</a></li> <!--main hoac main/index vi index se dong automaticlly-->
 <li><a href="<?php echo site_url('customer/show_customers');?>">Show Customers</a></li> <!--PHP: thay cho http://localhost/ci_monday/index.php/customer/show_customers-->
 <li><a href="<?php echo site_url('customer/add_customers');?>">Add a Customer</a></li>
 <li><a href="<?php echo site_url('customer/delete_customers');?>">Delete a Customer</a></li>
